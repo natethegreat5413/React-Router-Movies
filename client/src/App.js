83dec388f16006a5f0148f0898d338f4f74f1dce
Nathan Cowley <nathancowley12@gmail.com>
2020-04-20 14:40:17 -0600

    met MVP. Im able to navigate from each card back to home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -10,10 +13,21 @@ const App = () => {
   };
 
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+
+<div className='container'>
+    <SavedList list={savedList} />
+
+<div>
+    <Route exact path='/'>  
+      <MovieList />
+    </Route>
+
+    <Route path='/movies/:id'>
+      <Movie />
+    </Route>
+</div>
+</div>
+
   );
 };
 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { link, Link } from 'react-router-dom'
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +22,10 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <>
         <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>Movie Details</Link>
+        </>
       ))}
     </div>
   );
@@ -30,7 +34,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <div to='/movies/:id' className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
